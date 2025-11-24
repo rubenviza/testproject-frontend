b43959b6381bae2aeda4038b85af6c4311a0f259
Ruben Viza <ruviza@hotmail.com>
2025-11-24 12:00:43 +0100

    header clients

--- libs/modules/clients/src/lib/components/landing-clients/landing-clients.ts
+++ libs/modules/clients/src/lib/components/landing-clients/landing-clients.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, computed, inject } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { isNumber } from '@frontend/utilities';
@@ -10,14 +10,17 @@ import {
   distinctUntilChanged,
   of,
   switchMap,
+  tap,
 } from 'rxjs';
+import { listColor } from '../../constants/list-color';
 import { Client } from '../../models/client';
 import { ClientsService } from '../../services/clients.service';
+import { HeaderClientsComponent } from '../header-clients/header-clients';
 
 @Component({
   selector: 'lib-clients',
   templateUrl: './landing-clients.html',
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, HeaderClientsComponent],
   providers: [ClientsService],
 })
 export class LandingClientsComponent {
@@ -27,9 +30,16 @@ export class LandingClientsComponent {
 
   protected clientInput = new FormControl('');
 
-  protected clients = toSignal(this.clientsService.getClients(), {
-    initialValue: [] as Client[],
-  });
+  protected listColor = listColor;
+
+  protected loading = signal(true);
+
+  protected clients = toSignal(
+    this.clientsService.getClients().pipe(tap(() => this.loading.set(false))),
+    {
+      initialValue: [] as Client[],
+    }
+  );
 
   protected clientSearchItem = computed(() => {
     const client = this.clientSearch();
---
+++ libs/modules/clients/src/lib/constants/list-color.ts
@@ -0,0 +1,5 @@
+export const listColor = {
+  green: 'text-green-600',
+  purple: 'text-purple-600',
+  blue: 'text-blue-600',
+};
